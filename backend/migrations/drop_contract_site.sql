6c71d184b3993f0677504626e2e0bec1892ab0c8
Pat-piyapat <piyapat.suwancharoen@gmail.com>
2026-04-21 09:39:33 +0700

    ไม่บอก

--- backend/migrations/drop_contract_site.sql
+++
@@ -1,5 +0,0 @@
--- ลบตาราง contract_site (ใช้แค่ contract_device + SLid แล้ว)
--- แนะนำ: สำรองข้อมูลก่อน (SELECT * FROM contract_site; หรือ export)
--- รัน: mysql -u USER -p DB_NAME < migrations/drop_contract_site.sql
-
-DROP TABLE IF EXISTS contract_site;
